edb335893f1f39949963d24e480d8996a3ef2977
David <dasilvadav07@gmail.com>
2021-11-30 16:49:51 +0100

    ajout menu burger

--- main.js
+++ main.js
@@ -26,3 +26,25 @@ for (let i = 0; i < secondBubble.length; i++) {
     thirdBubble[i].style.left = '-20%';
 
 }
+
+let menuBtn = document.querySelector("#menu-mobile");
+    let firstBar = document.querySelector("#menu-mobile span:first-child");
+    let secondBar = document.querySelector("#menu-mobile span:nth-child(2)");
+    let thirdBar = document.querySelector("#menu-mobile span:last-child");
+    let menuLink = document.querySelectorAll('.menu-list-item a');
+    let menu = document.querySelector(".menu_open");
+    menuLink.forEach(link => {
+      link.addEventListener("click", () => {
+        firstBar.classList.toggle('transition');
+        secondBar.classList.toggle('transition2');
+        thirdBar.classList.toggle('transition3');
+        menu.classList.toggle('show');
+      })
+    });
+
+    menuBtn.addEventListener("click", () => {
+      firstBar.classList.toggle('transition');
+      secondBar.classList.toggle('transition2');
+      thirdBar.classList.toggle('transition3');
+      menu.classList.toggle('show');
+    })
