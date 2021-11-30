01d6279f0754673b8b899665d85c084834f6938e
David <dasilvadav07@gmail.com>
2021-11-30 15:49:08 +0100

    new html, css, js

--- main.js
+++ main.js
@@ -11,16 +11,18 @@ function positionCercle(element, theta) {
 for (let i = 0; i < cercles.length; i++) {
     let angle = ((Math.PI * 2) / cercles.length) * i;
     positionCercle(cercles[i], angle);    
+
+    cercles[i].querySelector('.bubble_comp_container').style.transform = `rotate(${224 + (i * 24)}deg)`;
 }
 
 for (let i = 0; i < secondBubble.length; i++) {
     let angle = ((Math.PI * 2) / secondBubble.length) * i;
 
-    secondBubble[i].style.left = cercles[i].style.left;
-    secondBubble[i].style.top = cercles[i].style.top;
-    firstBubble[i].style.top = cercles[i].style.top;
-    firstBubble[i].style.left = cercles[i].style.left;
-    thirdBubble[i].style.top = cercles[i].style.top;
-    thirdBubble[i].style.left = cercles[i].style.left;
+    secondBubble[i].style.left = 0;
+    secondBubble[i].style.top = '100%';
+    firstBubble[i].style.top = '120%';
+    firstBubble[i].style.left = '50%';
+    thirdBubble[i].style.top = '50%';
+    thirdBubble[i].style.left = '-20%';
 
 }
