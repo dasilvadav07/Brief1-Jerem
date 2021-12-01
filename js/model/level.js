fb3e3b6acb502370be2628ca764e2570d400d558
Alicia <aliciaderradji@live.fr>
2021-12-01 15:01:55 +0100

    isValidated

--- js/model/level.js
+++ js/model/level.js
@@ -4,12 +4,14 @@ class Level {
   number;
   skillId;
   userId;
-  constructor(id, label, number, skillId, userId) {
+  isValidated;
+  constructor(id, label, number, skillId, userId, isValidated) {
     this.id = id;
     this.label = label;
     this.number = number;
     this.skillId = skillId;
     this.userId = userId;
+    this.isValidated = isValidated;
   }
 }
 
